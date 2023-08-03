359dd3c598f1c68b3a630859ceca0906bc696bb5
Suetam777 <mateuschapad33@gmail.com>
2023-08-03 14:50:51 -0300

    página index adicionada

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'cabecalho.php'; ?>
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-8">
+                <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="https://media.istockphoto.com/id/1221267331/pt/foto/food-plate-with-meat-stroganoff-with-rice-and-french-fries-wood-background-flat-lay.jpg?s=2048x2048&w=is&k=20&c=dLU-tQBB2q04m2DCIL_7EJ1N5cB-F3gU-UfQxVioqbY=" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="https://images.pexels.com/photos/1059943/pexels-photo-1059943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                    </div>
+                     </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include 'rodape.php'; ?>
